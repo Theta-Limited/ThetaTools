507c3ecd744c8b997367528c081e05a3dee6b44b
Bobby Krupczak <rdk@krupczak.org>
2024-10-21 16:49:15 -0400

    Checkpoint.  GenerateApiKey new tool for generating an OA API key json object for use with OA Core API Server.  Testing scripts.

--- getApiConfig.sh
+++ getApiConfig.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# get OACore RESTful API stats
+# get OACore RESTful API configuration
 
 # pull in API key from env variable
 # to obtain stats, api key must have admin permissions
 
 API_KEY=$OPENATHENA_API_KEY
 
-URL="http://localhost:8000/api/v1/openathena/admin/stats?apikey=$API_KEY"
+URL="http://localhost:8000/api/v1/openathena/admin/config?apikey=$API_KEY"
 
 CURL_CMD="curl -s $URL"
 
--- runXtimes.sh
+++ runXtimes.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 # Number of times to run the curl command
-#NUM_REQUESTS=1000
-NUM_REQUESTS=100000
+NUM_REQUESTS=1000
+# NUM_REQUESTS=100000
 
 # pull in API key from env variable
 API_KEY=$OPENATHENA_API_KEY
 
 JSON_FILE="image.json"
-URL="http://localhost:8000/api/v1/openathena/location?apikey=$API_KEY"
+URL="http://localhost:8000/api/v1/openathena/locationsimple?apikey=$API_KEY"
 
 CURL_CMD="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' >& /dev/null "
+#CURL_CMD="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' "
 
 echo "Curl command is $CURL_CMD"
 
@@ -20,7 +21,8 @@ START_TIME=$(date +%s)
 # Run curl command multiple times
 for ((i=1; i<=NUM_REQUESTS; i++))
 do
-    eval $CURL_CMD
+    output=$(eval $CURL_CMD)
+    # echo $output
 done
 
 # End time
