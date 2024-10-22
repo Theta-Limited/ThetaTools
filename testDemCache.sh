9533ada54c403d5e28b2aaef4852e87aeebbaf1e
Bobby Krupczak <rdk@krupczak.org>
2024-10-22 17:26:56 -0400

    Checkpoint for new tests.

--- testDemCache.sh
+++ testDemCache.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
 # Number of times to run the curl command
-#NUM_REQUESTS=1000
-NUM_REQUESTS=100000
+NUM_REQUESTS=10
+# NUM_REQUESTS=100000
 
 # pull in API key from env variable
 API_KEY=$OPENATHENA_API_KEY
 
 JSON_FILE="image.json"
+JSON_FILE1="image1.json"
+JSON_FILE2="image2.json"
 URL="http://localhost:8000/api/v1/openathena/locationsimple?apikey=$API_KEY"
 
 CURL_CMD="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' >& /dev/null "
-#CURL_CMD="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' "
+CURL_CMD1="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE1} '$URL' >& /dev/null "
+CURL_CMD2="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE2} '$URL' >& /dev/null "
 
 echo "Curl command is $CURL_CMD"
 
@@ -22,7 +25,9 @@ START_TIME=$(date +%s)
 for ((i=1; i<=NUM_REQUESTS; i++))
 do
     output=$(eval $CURL_CMD)
-    # echo $output
+    output=$(eval $CURL_CMD1)
+    output=$(eval $CURL_CMD2)        
+
 done
 
 # End time
