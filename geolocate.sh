a774e812441d1f8916dbcfb7228779b636e4d3ba
Bobby Krupczak <rdk@krupczak.org>
2024-11-14 20:00:18 -0500

    Added geolocate.sh script that invokes restful api.

---
+++ geolocate.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# take a json image metadata file and geolocate via OpenAthenaCore
+# REST server
+
+# pull in API key from env variable
+API_KEY=$OPENATHENA_API_KEY
+
+JSON_FILE="$1"
+HOST="$2"
+PORT="$3"
+
+# check to make sure we have all the args we need
+if (( $# != 3)); then
+    echo "Usage: geolocate.sh image-json-file host port"
+    exit
+fi
+
+URL="http://$HOST:$PORT/api/v1/openathena/locationsimple?apikey=$API_KEY"
+
+CURL_CMD="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' "
+
+output=$(eval $CURL_CMD)
+
+echo $output
+echo
