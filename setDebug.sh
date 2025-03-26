8137aebbc7adbb5d658de196afdf2ff3de8b6a71
Bobby Krupczak <rdk@krupczak.org>
2025-03-26 14:06:10 -0400

    Fixed setDebug.sh

--- setDebug.sh
+++ setDebug.sh
@@ -18,13 +18,10 @@ API_KEY=$OPENATHENA_API_KEY
 
 URL="http://$HOST:$PORT/api/v1/openathena/admin/debug?apikey=$API_KEY"
 
-curl -X PUT \
-     -H "Content-Type: application/json" \
-     -d '{"value":"$BOOL"}' \
-     $URL
+curl_cmd="curl -X PUT -H \"Content-Type: application/json\" -d '{\"value\":\"$BOOL\"}' \"$URL\""
 
-curl_cmd="curl -X PUT -H \"Content-Type: application/json\" -d '{"value":"true"}' \"$URL\""
+echo "Executing command: $curl_cmd"
 
-output=$(eval $CURL_CMD)
+output=$(eval $curl_cmd)
 echo "$output" | jq 'to_entries | sort_by(.key) | from_entries'
 echo
