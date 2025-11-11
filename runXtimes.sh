aee7f019b0a6edaedcc39c11cd353be108c0d68f
Bobby Krupczak <rdk@krupczak.org>
2025-11-11 16:25:13 -0500

    Update image.json with dataset option.  Updated runXtimes to take X as arg and to do a trial run first to display return value before timing run.

--- runXtimes.sh
+++ runXtimes.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 # Number of times to run the curl command
-#NUM_REQUESTS=10000
-NUM_REQUESTS=10000
+# take from command-line arg
+# run 0th time to see the request/reply
+# NUM_REQUESTS=10000
+# NUM_REQUESTS=10
+
+NUM_REQUESTS="$1"
+
+if (( $# != 1)); then
+    echo "Usage: runXtimes.sh numRequests"
+    exit
+fi
 
 # pull in API key from env variable
 API_KEY=$OPENATHENA_API_KEY
@@ -10,10 +19,13 @@ API_KEY=$OPENATHENA_API_KEY
 JSON_FILE="image.json"
 URL="http://localhost:8000/api/v1/openathena/locationsimple?apikey=$API_KEY"
 
-CURL_CMD="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' >& /dev/null "
-#CURL_CMD="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' "
+CURL_CMD_NOOUT="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' >& /dev/null "
+CURL_CMD_OUTPUT="curl -s -X POST -H 'Content-Type: application/json' -d @${JSON_FILE} '$URL' "
 
 echo "Curl command is $CURL_CMD"
+
+output=$(eval $CURL_CMD_OUTPUT)
+echo $output
 
 # Start time
 START_TIME=$(date +%s)
@@ -21,7 +33,7 @@ START_TIME=$(date +%s)
 # Run curl command multiple times
 for ((i=1; i<=NUM_REQUESTS; i++))
 do
-    output=$(eval $CURL_CMD)
+    output=$(eval $CURL_CMD_NOOUT)
     # echo $output
 done
 
